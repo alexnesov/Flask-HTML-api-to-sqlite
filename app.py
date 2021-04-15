4fd7afed60893fc87a4d23a6fe782d8fde224fd3
alexandre nesovic <alexandrenesovic@gmail.com>
2021-04-15 20:22:59 +0200

    debug getcsv

--- app.py
+++ app.py
@@ -8,11 +8,16 @@ import os
 app = Flask(__name__, static_url_path='/static')
 
 sql_output = []
-currentWD = os.path.dirname(__file__)
+SQL_COMMAND = ""
+currentWD = os.path.dirname(__file__)  # WD = working directory
+pathToDB = os.path.join(currentWD, "utils\\marketdataSQL.db")
+print("here: ", pathToDB)
 
 
-def readSqlite(SQL_COMMAND, path='utils/marketdataSQL.db'):
-    conn = sqlite3.connect('utils/marketdataSQL.db')
+def readSqlite(SQL_COMMAND, path=pathToDB):
+    # conn = sqlite3.connect('utils/marketdataSQL.db')
+    conn = sqlite3.connect(f'{pathToDB}')
+
     mycur = conn.cursor()
     mycur.execute(f"{SQL_COMMAND}")
     sql_output = (mycur.fetchall())
@@ -27,37 +32,22 @@ def readSqlite(SQL_COMMAND, path='utils/marketdataSQL.db'):
 def home():
     return render_template('mainpage.html',
                            currentWD=currentWD,
+                           pathToDB=pathToDB,
                            valid=True)
-
-
-@app.route("/getCSV", methods=['GET'])
-def getCSV():
-
-    df = pd.DataFrame(sql_output)
-    print(df)
-
-    try:
-        df.to_csv(f"{currentWD}\\test.csv")
-    except PermissionError:
-        print(
-            'File used by another person, yourself, or simply not authorized to overwrite')
-
-    return Response(
-        df,
-        mimetype="text/csv",
-        headers={"Content-disposition":
-                 "attachment; filename=output.csv"})
 
 
 # to do: init command to avoid red arrow
 
+
 @app.route('/', methods=['POST'])
 def executeSQL():
+    global SQL_COMMAND
+    global sql_output
+
     SQL_COMMAND = request.form['textarea']
 
     if SQL_COMMAND:
         try:
-            global sql_output
             sql_output, colNames = readSqlite(SQL_COMMAND=SQL_COMMAND)
             valid = True
             widthDF = list(range(len(colNames)))
@@ -68,7 +58,8 @@ def executeSQL():
                 colNames=colNames,
                 widthDF=widthDF,
                 valid=valid,
-                currentWD=currentWD)
+                currentWD=currentWD,
+                pathToDB=pathToDB)
         except sqlite3.OperationalError:
             # If error in sql comand
             print("error in the SQL command")
@@ -76,6 +67,7 @@ def executeSQL():
             return render_template('mainpage.html',
                                    valid=valid,
                                    currentWD=currentWD,
+                                   pathToDB=pathToDB,
                                    SQL_COMMAND=SQL_COMMAND)
     else:
         print("Empty command")
@@ -83,6 +75,74 @@ def executeSQL():
         return render_template('mainpage.html',
                                valid=valid,
                                currentWD=currentWD,
+                               pathToDB=pathToDB,
+                               SQL_COMMAND=SQL_COMMAND)
+
+
+@app.route('/getCSV')
+def test():
+
+    print("HERE")
+    sql_output, colNames = readSqlite(SQL_COMMAND=SQL_COMMAND)
+    df = pd.DataFrame(sql_output)
+    widthDF = list(range(len(colNames)))
+
+    print("command: ", SQL_COMMAND)
+
+    print(df)
+    try:
+        df.to_csv(f"{currentWD}\\test.csv")
+    except PermissionError:
+        print(
+            'File used by another person, yourself, or simply not authorized to overwrite')
+
+    return render_template('mainpage.html',
+                           currentWD=currentWD,
+                           pathToDB=pathToDB,
+                           valid=True,
+                           SQL_COMMAND=SQL_COMMAND,
+                           sql_output=sql_output,
+                           colNames=colNames,
+                           widthDF=widthDF)
+
+
+@app.route("/getCSV", methods=['POST'])
+def getCSV():
+    global sql_output
+    global SQL_COMMAND
+    print('NOW')
+    SQL_COMMAND = request.form['textarea']
+
+    if SQL_COMMAND:
+        try:
+            sql_output, colNames = readSqlite(SQL_COMMAND=SQL_COMMAND)
+            valid = True
+            widthDF = list(range(len(colNames)))
+            return render_template(
+                'mainpage.html',
+                SQL_COMMAND=SQL_COMMAND,
+                sql_output=sql_output,
+                colNames=colNames,
+                widthDF=widthDF,
+                valid=valid,
+                currentWD=currentWD,
+                pathToDB=pathToDB)
+        except sqlite3.OperationalError:
+            # If error in sql comand
+            print("error in the SQL command")
+            valid = False
+            return render_template('mainpage.html',
+                                   valid=valid,
+                                   currentWD=currentWD,
+                                   pathToDB=pathToDB,
+                                   SQL_COMMAND=SQL_COMMAND)
+    else:
+        print("Empty command")
+        valid = False
+        return render_template('mainpage.html',
+                               valid=valid,
+                               currentWD=currentWD,
+                               pathToDB=pathToDB,
                                SQL_COMMAND=SQL_COMMAND)
 
 
