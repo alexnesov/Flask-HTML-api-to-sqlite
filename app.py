89415039283445e919ffa38cb08b6667600e6cf5
alexandre nesovic <alexandrenesovic@gmail.com>
2021-04-16 00:02:03 +0200

    user input db

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ app = Flask(__name__, static_url_path='/static')
 
 sql_output = []
 SQL_COMMAND = ""
+DB_USER_INPUT = ""
+INIT = True
 currentWD = os.path.dirname(__file__)  # WD = working directory
-pathToDB = os.path.join(currentWD, "utils\\marketdataSQL.db")
-print("here: ", pathToDB)
+#pathToDB = os.path.join(currentWD, "utils\\marketdataSQL.db")
 
 
-def readSqlite(SQL_COMMAND, path=pathToDB):
-    # conn = sqlite3.connect('utils/marketdataSQL.db')
-    conn = sqlite3.connect(f'{pathToDB}')
+def readSqlite(dbpath, SQL_COMMAND):
 
+    conn = sqlite3.connect(f'{dbpath}')
     mycur = conn.cursor()
     mycur.execute(f"{SQL_COMMAND}")
     sql_output = (mycur.fetchall())
@@ -32,65 +32,88 @@ def readSqlite(SQL_COMMAND, path=pathToDB):
 def home():
     return render_template('mainpage.html',
                            currentWD=currentWD,
-                           pathToDB=pathToDB,
+                           DB_USER_INPUT=DB_USER_INPUT,
                            valid=True)
 
-
 # to do: init command to avoid red arrow
+
+
+def STD_FUNC_TRUE(dbpath):
+    """
+    """
+
+    sql_output, colNames = readSqlite(dbpath, SQL_COMMAND=SQL_COMMAND)
+    widthDF = list(range(len(colNames)))
+    df = pd.DataFrame(sql_output)
+
+    standard_args = dict(
+        sql_output=sql_output,
+        colNames=colNames,
+        widthDF=widthDF,
+        valid=True,
+        currentWD=currentWD,
+        DB_USER_INPUT=DB_USER_INPUT
+    )
+
+    return standard_args, df
+
+
+def STD_FUNC_FALSE():
+
+    standard_args = dict(
+        currentWD=currentWD,
+        valid=False,
+        DB_USER_INPUT=DB_USER_INPUT
+    )
+
+    return standard_args
 
 
 @app.route('/', methods=['POST'])
 def executeSQL():
     global SQL_COMMAND
     global sql_output
+    global DB_USER_INPUT
+    global INIT
 
     SQL_COMMAND = request.form['textarea']
-    db_user_input = request.form.get('dbPathForm')
-    print(db_user_input)
+    if INIT == True:
+        print('init true')
+        DB_USER_INPUT = request.form.get('dbPathForm')
+
+    print('user input:', DB_USER_INPUT)
 
     if SQL_COMMAND:
         try:
-            sql_output, colNames = readSqlite(SQL_COMMAND=SQL_COMMAND)
-            valid = True
-            widthDF = list(range(len(colNames)))
+            std_args, df = STD_FUNC_TRUE(dbpath=DB_USER_INPUT)
+            INIT = False
+
             return render_template(
                 'mainpage.html',
                 SQL_COMMAND=SQL_COMMAND,
-                sql_output=sql_output,
-                colNames=colNames,
-                widthDF=widthDF,
-                valid=valid,
-                currentWD=currentWD,
-                pathToDB=pathToDB)
+                **std_args)
         except sqlite3.OperationalError:
             # If error in sql comand
             print("error in the SQL command")
-            valid = False
+            std_args = STD_FUNC_FALSE()
+
             return render_template('mainpage.html',
-                                   valid=valid,
-                                   currentWD=currentWD,
-                                   pathToDB=pathToDB,
-                                   SQL_COMMAND=SQL_COMMAND)
+                                   SQL_COMMAND=SQL_COMMAND,
+                                   **std_args)
     else:
         print("Empty command")
-        valid = False
+        std_args = STD_FUNC_FALSE()
+
         return render_template('mainpage.html',
-                               valid=valid,
-                               currentWD=currentWD,
-                               pathToDB=pathToDB,
-                               SQL_COMMAND=SQL_COMMAND)
+                               SQL_COMMAND=SQL_COMMAND,
+                               **std_args)
 
 
 @app.route('/getCSV')
 def test():
 
     print("HERE")
-    sql_output, colNames = readSqlite(SQL_COMMAND=SQL_COMMAND)
-    df = pd.DataFrame(sql_output)
-    widthDF = list(range(len(colNames)))
-
-    print("command: ", SQL_COMMAND)
-
+    std_args, df = STD_FUNC_TRUE(dbpath=DB_USER_INPUT)
     print(df)
     try:
         df.to_csv(f"{currentWD}\\test.csv")
@@ -99,53 +122,41 @@ def test():
             'File used by another person, yourself, or simply not authorized to overwrite')
 
     return render_template('mainpage.html',
-                           currentWD=currentWD,
-                           pathToDB=pathToDB,
-                           valid=True,
                            SQL_COMMAND=SQL_COMMAND,
-                           sql_output=sql_output,
-                           colNames=colNames,
-                           widthDF=widthDF)
+                           **std_args)
 
 
 @app.route("/getCSV", methods=['POST'])
 def getCSV():
-    global sql_output
     global SQL_COMMAND
+    global sql_output
+
     print('NOW')
     SQL_COMMAND = request.form['textarea']
 
     if SQL_COMMAND:
         try:
-            sql_output, colNames = readSqlite(SQL_COMMAND=SQL_COMMAND)
-            valid = True
-            widthDF = list(range(len(colNames)))
+            std_args, df = STD_FUNC_TRUE(dbpath=DB_USER_INPUT)
+
             return render_template(
                 'mainpage.html',
                 SQL_COMMAND=SQL_COMMAND,
-                sql_output=sql_output,
-                colNames=colNames,
-                widthDF=widthDF,
-                valid=valid,
-                currentWD=currentWD,
-                pathToDB=pathToDB)
+                **std_args)
         except sqlite3.OperationalError:
             # If error in sql comand
             print("error in the SQL command")
-            valid = False
+            std_args = STD_FUNC_FALSE()
+
             return render_template('mainpage.html',
-                                   valid=valid,
-                                   currentWD=currentWD,
-                                   pathToDB=pathToDB,
-                                   SQL_COMMAND=SQL_COMMAND)
+                                   SQL_COMMAND=SQL_COMMAND,
+                                   **std_args)
     else:
         print("Empty command")
-        valid = False
+        std_args = STD_FUNC_FALSE()
+
         return render_template('mainpage.html',
-                               valid=valid,
-                               currentWD=currentWD,
-                               pathToDB=pathToDB,
-                               SQL_COMMAND=SQL_COMMAND)
+                               SQL_COMMAND=SQL_COMMAND,
+                               **std_args)
 
 
 if __name__ == '__main__':
